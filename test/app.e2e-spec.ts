cbde50cb0e29d47c8e639ded063347342742c3b2
Muhammed Ali <madilekci@gmail.com>
2023-03-20 15:57:09 +0300

    Add tests for bookmarks

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import * as pactum from 'pactum';
 import { PrismaService } from './../src/prisma/prisma.service';
 import { AppModule } from './../src/app.module';
 import { AuthDto } from './../src/auth/dto/auth.dto';
+import { CreateBookmarkDto, EditBookmarkDto } from './../src/bookmark/dto';
 
 describe('app e2e', () => {
     let app: INestApplication;
@@ -93,6 +94,112 @@ describe('app e2e', () => {
         });
     });
 
+    describe('Bookmarks', () => {
+      describe('Get empty bookmarks', () => {
+        it('should get bookmarks', () => {
+          return pactum
+            .spec()
+            .get('/bookmarks')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .expectStatus(200)
+            .expectBody([]);
+        });
+      });
+
+      describe('Create bookmark', () => {
+        const dto: CreateBookmarkDto = {
+          title: 'First Bookmark',
+          link: 'https://www.youtube.com/watch?v=d6WC5n9G_sM',
+        };
+        it('should create bookmark', () => {
+          return pactum
+            .spec()
+            .post('/bookmarks')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .withBody(dto)
+            .expectStatus(201)
+            .stores('bookmarkId', 'id');
+        });
+      });
+
+      describe('Get bookmarks', () => {
+        it('should get bookmarks', () => {
+          return pactum
+            .spec()
+            .get('/bookmarks')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .expectStatus(200)
+            .expectJsonLength(1);
+        });
+      });
+
+      describe('Get bookmark by id', () => {
+        it('should get bookmark by id', () => {
+          return pactum
+            .spec()
+            .get('/bookmarks/{id}')
+            .withPathParams('id', '$S{bookmarkId}')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .expectStatus(200)
+            .expectBodyContains('$S{bookmarkId}');
+        });
+      });
+
+      describe('Edit bookmark by id', () => {
+        const dto: EditBookmarkDto = {
+          title:
+            'Kubernetes Course - Full Beginners Tutorial (Containerize Your Apps!)',
+          description:
+            'Learn how to use Kubernetes in this complete course. Kubernetes makes it possible to containerize applications and simplifies app deployment to production.',
+        };
+        it('should edit bookmark', () => {
+          return pactum
+            .spec()
+            .patch('/bookmarks/{id}')
+            .withPathParams('id', '$S{bookmarkId}')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .withBody(dto)
+            .expectStatus(200)
+            .expectBodyContains(dto.title)
+            .expectBodyContains(dto.description);
+        });
+      });
+
+      describe('Delete bookmark by id', () => {
+        it('should delete bookmark', () => {
+          return pactum
+            .spec()
+            .delete('/bookmarks/{id}')
+            .withPathParams('id', '$S{bookmarkId}')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .expectStatus(204);
+        });
+
+        it('should get empty bookmarks', () => {
+          return pactum
+            .spec()
+            .get('/bookmarks')
+            .withHeaders({
+              Authorization: 'Bearer $S{userAccessToken}',
+            })
+            .expectStatus(200)
+            .expectJsonLength(0);
+        });
+      });
+    });
+
     describe('User', () => {
         describe('Get Me', () => {
           it('should get current user', () => {
@@ -120,15 +227,6 @@ describe('app e2e', () => {
                     .expectStatus(200);
             });
         });
-    });
-
-    describe('Bookmarks', () => {
-        describe('Create bookmark', () => {});
-        describe('Get bookmarks', () => {});
-        describe('Get bookmark by id', () => {});
-
-        describe('Update bookmark by id', () => {});
-        describe('Delete bookmark by id', () => {});
     });
 
     afterAll(() => {
